ff4935aac1602639a6269ff90dd2c88fdd2c6ee8
Fernanda Daza <fernanditadaza9@gmail.com>
2024-11-08 17:40:09 -0300

    mejoras en index

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "usuarios"
 
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),  # Usa LoginView directamente
-    path('inventario/', views.inventario, name='inventario'),
+    path('', views.login_view, name='login'),  # Usa LoginView directamente
+    path('inicio/', views.index, name='index'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,27 @@
-
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
-def login(request):
-    return render(request, 'login/login.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('usuarios:index')  # Redirige a la página de inicio después de iniciar sesión
+            else:
+                messages.error(request, 'Nombre de usuario o contraseña incorrectos')
+        else:
+            messages.error(request, 'Formulario no válido')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login/login.html', {'form': form})
 
 
-
-@login_required
-def inventario(request):
+def index(request):
     return render(request, 'index.html')
