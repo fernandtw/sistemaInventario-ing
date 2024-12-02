c5a5d1882c63262d1c5ae3909d35c1cc7e32a12d
Dorcha88 <dorchalotus@gmail.com>
2024-12-01 21:30:23 -0300

    arreglos de producto

--- productos/forms.py
+++ productos/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Producto
 
 class ProductoForm(forms.ModelForm):
+    fecha_vencimiento = forms.DateField(
+        label="Fecha Vencimiento",
+        required=False,  # Cambia a False si el campo es opcional
+        widget=forms.DateInput(format="%Y-%m-%d", attrs={"type": "date"}),
+        input_formats=["%Y-%m-%d"]
+    )
+
     class Meta:
         model = Producto
-        fields = ['nombre', 'descripcion', 'precio', 'cantidad']
+        fields = ['nombre', 'descripcion', 'precio', 'cantidad', 'fecha_vencimiento']
+ 
+
